f91d259d1c6d5bcc5d8d424773b592e838b53677
AnantAgrawal29 <agrawalanant2021@gmail.com>
2026-08-16 23:33:26 +0530

    initial commit

---
+++ data/database.js
@@ -0,0 +1,26 @@
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+let database;
+let mongoUrl = "mongodb://localhost:27017";
+
+if (process.env.MONGODB_URL) mongodbUrl = process.env.MONGODB_URL;
+
+async function initDatabase() {
+  const client = await MongoClient.connect(mongoUrl);
+  database = client.db("deployment");
+}
+
+function getDb() {
+  if (!database) {
+    throw new Error("No database connected!");
+  }
+
+  return database;
+}
+
+module.exports = {
+  initDatabase: initDatabase,
+  getDb: getDb,
+};
